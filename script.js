c49bb6a337f746a0d4394d8fbd1b3c05d4612380
David Long <yijunlong57@gmail.com>
2025-01-13 21:56:25 +1000

    finished calc interface

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const container = document.querySelector(".container");
 
-for (i = 0; i < 3; i++) {
+const numbersContainer = document.querySelector(".numbers-container");
+
+for (let i = 1; i < 10; i++) {
     let button = document.createElement("button");
-    button.classList.add("number_buttons");
-    button.textContent = i;
-    container.appendChild(button);
-    for (i = 0; i < 3; i++) {
-        let button = document.createElement("button");
-        button.classList.add("number_buttons");
-        button.textContent = i;
-        container.appendChild(button);
-    }
+    button.classList.add("number-buttons");
+    button.textContent = i; 
+    numbersContainer.appendChild(button);    
 }
+let button = document.createElement("button");
+    button.classList.add("number-buttons");
+    button.textContent = 0; 
+    numbersContainer.appendChild(button);
+
+const operationsContainer = document.querySelector(".operations-container");
+
+let plus = document.createElement("button");
+    plus.classList.add("operations");
+    plus.textContent = "+"; 
+    operationsContainer.appendChild(plus);  
+
+let minus = document.createElement("button");
+minus.classList.add("operations");
+minus.textContent = "-"; 
+operationsContainer.appendChild(minus);
+
+let multiply = document.createElement("button");
+multiply.classList.add("operations");
+multiply.textContent = "x"; 
+operationsContainer.appendChild(multiply);
+
+let divide = document.createElement("button");
+divide.classList.add("operations");
+divide.textContent = "÷"; 
+operationsContainer.appendChild(divide);
+    
+    
+
